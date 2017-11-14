27c4c156cfd93b297e3f64856d0eb0a1094a7dac
Brian Choe <brian@visibook.com>
2017-11-13 20:29:00 -0800

    includes set up for eslint-airbnb

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,9 @@
  *
  * Visit this repo for more information:
  *   https://github.com/reactorcore/eslint-config-hackreactor
+ *  extends: './node_modules/eslint-config-hackreactor/index.js'
  */
 
 module.exports = {
-  extends: './node_modules/eslint-config-hackreactor/index.js'
+  "extends": "airbnb",
 };
---
+++ testFile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class newComponent extends React.Component {
+  constructor () {
+    super()
+  }
+
+  render() {
+    return (
+
+    )
+  }
+}

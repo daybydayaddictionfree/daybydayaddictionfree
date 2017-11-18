13993a51b505b6455eb967b76cb1ff16bcaa0f91
Brian Choe <brian@visibook.com>
2017-11-18 14:23:37 -0800

    installed and now using material ui dependency

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import messages from '../../../sampleData';
+import { Card, CardHeader, CardText } from 'material-ui/Card';
 
 const Message = () => (
-  <div>
-    <h1>Message Here</h1>
-    <div>{messages[0].sender} {messages[0].timeStamp}</div>
-    <div>{messages[0].text}</div>
-  </div>
+  <Card style={{ width: '66%', margin: '5px auto' }}>
+    <CardHeader
+      title={messages[0].sender}
+      subtitle={messages[0].timeStamp}
+    />
+    <CardText>{messages[0].text}</CardText>
+  </Card>
 );
 
 export default Message;
+
+// <div className="message">
+//   <div>
+//     <span className="sender">{messages[0].sender}</span>
+//     <span className="timeStamp">{messages[0].timeStamp}</span>
+//   </div>
+//   <div className="message-body">{messages[0].text}</div>
+// </div>
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import App from './components/App.jsx';
 
 ReactDOM.render(
   (
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <MuiThemeProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </MuiThemeProvider>
   ), document.getElementById('root')
 );

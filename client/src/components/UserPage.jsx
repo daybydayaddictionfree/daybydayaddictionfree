105e8953a973beac0e02fac060585bd1d5b2cc91
Brian Choe <brian@visibook.com>
2017-11-20 10:31:14 -0800

    minor tweaks to progress bar component for rendering

--- client/src/components/UserPage.jsx
+++ client/src/components/UserPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import MessageList from './MessageList.jsx'
 import messages from '../../../sampleData';
+import ProgressBar from './ProgressBar.jsx';
 
 const UserPage = () => (
   <div>
+    <ProgressBar />
     <MessageList messages={messages} />
   </div>
 );

2cd175cf794f1b42b4c4589736cb9a6fbe016c23
Brian Choe <brian@visibook.com>
2017-11-20 12:32:15 -0800

    refactoring login component to be stateful

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,18 +1,41 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 import GoogleLogin from 'react-google-login';
 import GoogleId from '../../../keysAndIds/GoogleId.js';
 
 const responseGoogle = (response) => {
   console.log(response);
+  console.log('in response function');
+
+  return (
+    <div>
+      <h1>Redirect Invoked</h1>
+      <Redirect to={{
+          pathname: '/user',
+        }}
+      />
+    </div>
+  );
 };
 
-const Login = () => (
-  <GoogleLogin
-    clientId={GoogleId.client_id}
-    buttonText="Login using Google"
-    onSuccess={responseGoogle}
-    onFailure={responseGoogle}
-  />
-);
+class Login extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <GoogleLogin
+        clientId={GoogleId.client_id}
+        buttonText="Login using Google"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+      />
+    )
+  }
+}
 
 export default Login;
--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const path = require('path');
+const session = require('express-session');
 
 const port = 8080;
 const app = express();
 
 console.log(__dirname);
+app.use(session({
+  secret: 'dbdsecretstring123!',
+  saveUnitialized: true,
+  resave: true,
+}));
+
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.get('*', (req, res) => {

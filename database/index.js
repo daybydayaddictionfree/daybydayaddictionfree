7df80005ce7311604641ed8417c6a46a003a7a79
Jason Borer <borerjason@gmail.com>
2017-11-20 16:05:23 -0800

    create smokers db

--- database/index.js
+++ database/index.js
@@ -10,8 +10,12 @@ client.connect()
   .then(() => console.log('Connected to postgresql database!'))
   .catch(e => console.log('connection error: ', e));
 
-client.query('DROP TABLE smokers')
-client.query('CREATE TABLE smokers (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), phone VARCHAR(15) UNIQUE, email VARCHAR(15), timezone VARCHAR(15), progress INT(15), reminder VARCHAR(15)');
+client.query('DROP TABLE smokers');
+client.query('CREATE TABLE IF NOT EXISTS smokers(id serial not null primary key, name varchar(64), phone VARCHAR(15) UNIQUE, mail VARCHAR(64), timezone VARCHAR(15), progress int, reminder VARCHAR(15) )');
+
+
+
+// , e, timezone VARCHAR(15), progress INT(15), reminder VARCHAR(15))');
 
 module.exports = {
   query: (text, params) =>

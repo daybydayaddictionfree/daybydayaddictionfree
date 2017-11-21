560110d7544b884a43d03062169a14c38dfa0853
Jason Borer <borerjason@gmail.com>
2017-11-20 17:20:12 -0800

    separate seed fcn

--- database/index.js
+++ database/index.js
@@ -1,8 +1,8 @@
 const { Client } = require('pg');
-const db = require('../keysAndIds/db.js');
+const connectionString = require('../keysAndIds/db.js');
 
 const client = new Client({
-  connectionString: db.connectionString,
+  connectionString,
   ssl: true,
 });
 
@@ -10,23 +10,25 @@ client.connect()
   .then(() => console.log('Connected to postgresql database!'))
   .catch(e => console.log('connection error: ', e));
 
+// create tables for database
 client.query('DROP TABLE smokers');
-
 client.query('CREATE TABLE IF NOT EXISTS smokers(id serial not null primary key, name varchar(64), phone VARCHAR(64) UNIQUE, email VARCHAR(64) UNIQUE, timezone VARCHAR(64), progress int, reminder VARCHAR(64) )');
 
 client.query('DROP TABLE friends');
-
 client.query('CREATE TABLE IF NOT EXISTS friends(id serial not null primary key, name varchar(64), phone VARCHAR(64) UNIQUE, id_smokers int)');
 
 client.query('DROP TABLE messages');
-
 client.query('CREATE TABLE IF NOT EXISTS messages (id serial not null primary key, text varchar(64), timestamp varchar (25), id_smokers int, id_friends int)');
 
-client.query('INSERT INTO smokers (name, phone, email, progress) VALUES ($1, $2, $3, $4)', ['Abraham Lincoln', '555-555-5555', 'abe@gmail.com', 0]);
 
-client.query('INSERT INTO friends(name, phone, id_smokers) VALUES ($1, $2, $3)', ['Mom', '333-333-3333', 1]);
 
-client.query('INSERT INTO messages(text, timestamp, id_smokers, id_friends) VALUES ($1, $2, $3, $4)', ['Goodjob sweetie!!!', '11/18/2017', 1, 1]);
+// Seed table with dummy data
+
+// client.query('INSERT INTO smokers (name, phone, email, progress) VALUES ($1, $2, $3, $4)', ['Abraham Lincoln', '555-555-5555', 'abe@gmail.com', 0]);
+
+// client.query('INSERT INTO friends(name, phone, id_smokers) VALUES ($1, $2, $3)', ['Mom', '333-333-3333', 1]);
+
+// client.query('INSERT INTO messages(text, timestamp, id_smokers, id_friends) VALUES ($1, $2, $3, $4)', ['Goodjob sweetie!!!', '11/18/2017', 1, 1]);
 
 
 client.query('SELECT * from friends')
@@ -38,4 +40,3 @@ module.exports = {
   query: (text, params) =>
     client.query(text, params),
 };
-

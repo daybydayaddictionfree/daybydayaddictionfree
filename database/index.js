1c933b4c343dbddb69f07842f6e80d409d9385ec
Jason Borer <borerjason@gmail.com>
2017-11-22 00:10:04 -0800

    send user and msg data back to user

--- database/index.js
+++ database/index.js
@@ -11,16 +11,16 @@ client.connect()
   .catch(e => console.log('connection error: ', e));
 
 // create tables for database
-client.query('DROP TABLE smokers');
+// client.query('DROP TABLE smokers');
 client.query('CREATE TABLE IF NOT EXISTS smokers(id serial not null primary key, name varchar(64), phone VARCHAR(64) UNIQUE, email VARCHAR(64) UNIQUE, timezone VARCHAR(64), progress int, responded boolean, admin boolean)');
 
-client.query('DROP TABLE friends');
+// client.query('DROP TABLE friends');
 client.query('CREATE TABLE IF NOT EXISTS friends(id serial not null primary key, name varchar(64), phone VARCHAR(64) UNIQUE, id_smokers int, phone_smokers VARCHAR(64) UNIQUE)');
 
-client.query('DROP TABLE messages');
+// client.query('DROP TABLE messages');
 client.query('CREATE TABLE IF NOT EXISTS messages (id serial not null primary key, text varchar(64), timestamp varchar (25), id_smokers int, id_friends int)');
 
-client.query('DROP TABLE cookies');
+// client.query('DROP TABLE cookies');
 client.query('CREATE TABLE IF NOT EXISTS cookies (id serial not null primary key, token varchar(1250), email VARCHAR(64) UNIQUE, id_smokers int)');
 
 
--- database/queries.js
+++ database/queries.js
@@ -12,18 +12,28 @@ const insertFriends = ((friends) => {
 const insertMessage = (message =>
   query('INSERT INTO messages(text, timestamp, id_smokers, id_friends) VALUES ($1, $2, $3, $4)', message));
 
-const retrieveUserInfo = (email =>
+const checkEmail = (email =>
   query('SELECT * FROM smokers WHERE email=$1', [email]));
 
 const checkCookie = (token =>
-  query('SELECT email FROM cookies WHERE token=$1', [token]));
+  query('SELECT id_smokers FROM cookies WHERE token=$1', [token]));
 
 const insertCookie = (({ token, email, id }) => 
   query('INSERT INTO cookies(token, email, id_smokers) VALUES ($1, $2, $3)', [token,email, id]));
 
+const retrieveUserInfo = (id =>
+  query('SELECT * FROM smokers WHERE id=$1', [id]));
+
+const retrieveMessages = (id =>
+  query('SELECT * from messages INNER JOIN friends ON friends.id=messages.id_friends WHERE messages.id_smokers=$1',[id]));
+
+  
+    
 module.exports.insertSmoker = insertSmoker;
 module.exports.insertFriends = insertFriends;
 module.exports.insertMessage = insertMessage;
-module.exports.retrieveUserInfo = retrieveUserInfo;
+module.exports.checkEmail = checkEmail;
 module.exports.checkCookie = checkCookie;
 module.exports.insertCookie = insertCookie;
+module.exports.retrieveUserInfo = retrieveUserInfo;
+module.exports.retrieveMessages = retrieveMessages;

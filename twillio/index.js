b40b79eb01e22e5be1fb1f38347ee309352feccb
I3EAST <asidhu@ucdavis.edu>
2017-11-22 15:16:15 -0800

    minor twillio

--- twillio/index.js
+++ twillio/index.js
@@ -6,14 +6,20 @@ const authToken = key;
 // require the Twilio module and create a REST client
 const client = require('twilio')(accountSid, authToken);
 
-const send = () => {
+const send = (number) => {
+  number = "+" + number;
   client.messages.create({
-    to: '+15106851561',
+    to: number,
     from: '+4156809196',
     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
   }, (err, message) => {
     console.log(message.sid);
   });
 };
+const sendSmokers = (array) => {
+  array.forEach((item) => {
+    send(item);
+  });
+};
 
-module.exports = send;
+module.exports = sendSmokers;

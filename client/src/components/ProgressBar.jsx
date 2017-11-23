d937602bc00fb05f67a56dac2123e02362c02ea2
Jason Borer <borerjason@gmail.com>
2017-11-23 09:50:14 -0800

    progress bar updates from database

--- client/src/components/ProgressBar.jsx
+++ client/src/components/ProgressBar.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import LinearProgress from 'material-ui/LinearProgress';
 
-const ProgressBar = (props) => (
+const ProgressBar = ({ progress }) => (
   <LinearProgress
     style={{ width: '66%', margin: '5px auto' }}
     mode="determinate"
     max={60}
-    value={this.props.progress}
+    value={progress}
   />);
 
 export default ProgressBar;

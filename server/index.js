29e3dcc49b71e792f682a837ebcffd2e957f3a8f
Brian Choe <brian@visibook.com>
2017-11-27 14:56:02 -0800

    completed server tests

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const cookiesMiddleWare = require('universal-cookie-express');
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
+const morgan = require('morgan');
 const parser = require('body-parser');
 let twilio = require('twilio');
 const { send, sendStatusToFriends, sendSmokerCheckins } = require('../twilio/index');
@@ -15,11 +17,13 @@ const db = require('../database/index.js');
 const port = process.env.PORT || 8080;
 const app = express();
 
+app.use(morgan('dev'));
+app.use(cors());
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.get('/verifyAuth', cookiesMiddleWare(), (req, res) => {
+app.get('/verifyauth', cookiesMiddleWare(), (req, res) => {
   console.log('Serving request type ', req.method, ' from ', req.path);
 
   // Checks if cookie is valid
@@ -44,7 +48,7 @@ app.get('/verifyAuth', cookiesMiddleWare(), (req, res) => {
           });
 
       // If cookie is not valid..
-      } else {
+      }  else {
         res.send(false);
       }
     });
